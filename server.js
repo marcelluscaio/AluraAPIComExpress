d1c57c57a01b868e8af6864101dc3eaa6e64be69
Caio Cabral <ocaiocabral@gmail.com>
2023-09-18 19:49:47 -0400

    feat: protecting access using dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* import http from "http"; */
-import app from "./src/app.js"
+import "dotenv/config";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
--- src/app.js
+++ src/app.js
@@ -1,30 +1,27 @@
 import express from "express";
+import connectWithDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const connection = await connectWithDatabase();
+
+connection.on("error", (error) => {
+  console.error("erro de conexão", error)
+});
+
+connection.once("open", () => {
+  console.log("Conexão com banco feita com sucesso")
+});
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "O senhor do Aneis"
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit"
-  }
-];
-function buscaLivro(id){
-  return livros.findIndex(livro => {
-    return livro.id === Number(id);
-  })
-};
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {

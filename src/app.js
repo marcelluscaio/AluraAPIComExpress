74565f9e0c9f5ac6c177ed87774ec7e6c73e7a4f
Caio Cabral <ocaiocabral@gmail.com>
2023-09-19 16:50:43 -0400

    feat: implementing barrel pattern on Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectWithDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./Routes/index.js";
 
 const connection = await connectWithDatabase();
 
@@ -13,16 +13,8 @@ connection.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
+routes(app);
 
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node.js");
-});
-
-/* app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-}); */
 
 app.get("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,7 +4,18 @@ class LivroController{
   static async listarLivros (req, res) {
     const listaLivros = await livro.find({});
     res.status(200).json(listaLivros);
+  };
+
+  static async cadastrarLivro(req, res){
+    try{
+      const novoLivro = await livro.create(req.body);
+      res.status(201).json({message: "Criado com sucesso", livro: novoLivro})
+
+    } catch (error){
+      res.status(500).json({message: `${error.message} - falha ao cadastrar livro`})
+    }
   }
 };
+
 
 export default LivroController;
